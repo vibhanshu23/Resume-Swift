dbb5b6c0e0250b3063d2cfeab532f84317547b53
vibhanshu23 <vibhanshu7ain@gmail.com>
2023-08-11 22:38:49 -0500

    Initial Commit

---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by Vibhanshu Jain on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
